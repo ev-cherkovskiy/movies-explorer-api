5e0eda2f7f18ab6b9adb58d016452723ca580851
Evgeny Cherkovskiy <ev.cherkovskiy@yandex.ru>
2022-07-02 03:40:19 +0300

    fix: добавить валидацию поля name

--- utils/validation.js
+++ utils/validation.js
@@ -19,7 +19,7 @@ const validateRegistration = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
   }),
 };
 
